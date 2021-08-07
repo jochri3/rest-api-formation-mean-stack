08ba3cc60788fc37ea03698c6c6c1104b2f3a475
Christian Lisangola <jochri3@gmail.com>
2021-08-07 23:08:25 +0100

    add fake database

--- server.js
+++ server.js
@@ -2,6 +2,27 @@ import express from "express";
 
 const app = express();
 
+const contacts = [
+  {
+    id: 1,
+    name: "Christophe Guéroult",
+    phone: "+33 6 12 34 56 78",
+    email: "c.gueroult@coderbase.io",
+  },
+  {
+    id: 2,
+    name: "Pierre-Nedelec",
+    phone: "+33 6 12 34 32 78",
+    email: "p.nedelec@coderbase.io",
+  },
+  {
+    id: 3,
+    name: "Christian Lisangola",
+    phone: "+33 9 87 34 32 78",
+    email: "christian@zabibu.co",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Bienvenu dans le serveur express");
 });
