86e3b24ed4bad27684c19d5960b7dbe75ec3bd99
Christian Lisangola <jochri3@gmail.com>
2021-08-07 23:00:39 +0100

    create basic express server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("welcome to express");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bienvenu dans le serveur express");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
