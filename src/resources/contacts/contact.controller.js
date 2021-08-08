d1dee7037e116d2a5a01132199780f38f01c50b4
Christian Lisangola <jochri3@gmail.com>
2021-08-08 22:23:53 +0100

    add findOne controller

--- src/resources/contacts/contact.controller.js
+++ src/resources/contacts/contact.controller.js
@@ -10,6 +10,19 @@ const findAll = async (req, res) => {
   }
 };
 
+const findOne = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const contact = await Contact.findById(id);
+    if (!contact) return res.status(404).send({ message: "Not Found" });
+    res.json(contact);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
 export default {
   findAll,
+  findOne,
 };
