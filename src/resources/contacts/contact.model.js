68ab4954ad95a54973a6db52093c7588f35a2999
Christian Lisangola <jochri3@gmail.com>
2021-08-08 18:10:34 +0100

    add contact model

---
+++ src/resources/contacts/contact.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const ContactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active",
+  },
+});
+
+export const Contact = mongoose.model("Contact", ContactSchema);
