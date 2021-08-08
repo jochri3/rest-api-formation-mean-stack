940f5712abf12347a38d5ea4f2c30dcac0db1fe7
Christian Lisangola <jochri3@gmail.com>
2021-08-08 21:52:31 +0100

    add contact routes middleware

--- src/app.js
+++ src/app.js
@@ -12,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/contacts", contactRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
