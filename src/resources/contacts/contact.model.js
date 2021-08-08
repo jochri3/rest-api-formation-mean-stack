8e90881dde3f444aeed7afac77389945b8a5aa34
Christian Lisangola <jochri3@gmail.com>
2021-08-08 21:56:47 +0100

    add timestamps in contact model

--- src/resources/contacts/contact.model.js
+++ src/resources/contacts/contact.model.js
@@ -1,25 +1,30 @@
 import mongoose from "mongoose";
 
-const ContactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const ContactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  status: {
-    type: String,
-    enum: ["active", "inactive"],
-    default: "active",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Contact = mongoose.model("Contact", ContactSchema);
