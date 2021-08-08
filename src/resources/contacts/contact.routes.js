28bc2e17cbf53a0c1e1957e80eeee6cdd19c64eb
Christian Lisangola <jochri3@gmail.com>
2021-08-08 18:20:46 +0100

    add contact routes

---
+++ src/resources/contacts/contact.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+const contactRoutes = Router();
+
+contactRoutes.get("/", (req, res) => {
+  res.status(200).json("Tous les contacts");
+});
+
+contactRoutes.get("/:id", (req, res) => {
+  res.status(200).json(`Un seul contact`);
+});
+
+contactRoutes.post("/", (req, res) => {
+  res.status(201).json(`Nouveau contact`);
+});
+
+contactRoutes.put("/:id", (req, res) => {
+  res.status(200).json(`Modification contact`);
+});
+
+contactRoutes.delete("/:id", (req, res) => {
+  res.status(200).json(`Suppression contact`);
+});
+
+export default contactRoutes;
