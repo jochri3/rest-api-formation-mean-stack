a88f78d169d379666753fb27a35a5ac8eab4a8f4
Christian Lisangola <jochri3@gmail.com>
2021-08-10 23:54:12 +0100

    refact:remove var usage

--- src/lib/validate.js
+++ src/lib/validate.js
@@ -2,17 +2,17 @@
 
 export class Validate {
   static isEmail(email) {
-    var regex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    const regex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     return regex.test(email);
   }
 
   static isValidName(name) {
-    var regex = /^[a-zA-Z]+[a-zA-Z ]+$/;
+    const regex = /^[a-zA-Z]+[a-zA-Z ]+$/;
     return regex.test(name);
   }
 
   static isValidPhone(phone) {
-    var regex = /^[0-9]{10,}$/;
+    const regex = /^[0-9]{10,}$/;
     return regex.test(phone);
   }
 
