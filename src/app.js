aed70c721a508174a19935c37355be641cdd83d8
Christian Lisangola <jochri3@gmail.com>
2021-08-15 17:51:06 +0100

    add filling form event in socket

--- src/app.js
+++ src/app.js
@@ -3,16 +3,30 @@ const cors = require('cors')
 const { connectWithRetry } = require('./services/mongodb.js')
 const { contactRoutes } = require('./router.js')
 
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {
+  cors: {
+    origin: ['*'],
+  },
+})
+
 connectWithRetry()
 
 const app = express()
 
+app.set('socketio', io)
 app.use(cors())
 app.use(express.json())
 app.use('/api/contacts', contactRoutes)
 
 app.all('*', (_, res) => {
   res.sendStatus(404)
+})
+
+io.on('connection', (socket) => {
+  socket.on('filling-form', () => {
+    socket.emit.broadcast('other-user-filling-form')
+  })
 })
 
 const PORT = process.env.PORT || 3000
