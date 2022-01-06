8c91e93b48ce76b6d64e0513c17cff472a7dcc6b
Christian Lisangola <jochri3@gmail.com>
2022-01-06 17:53:35 +0300

    add user model

---
+++ src/resources/user/user.model.js
@@ -0,0 +1,44 @@
+const { mongoose } = require('../../services/mongodb.js')
+const jwt = require('jsonwebtoken')
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+UserSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      isAdmin: this.isAdmin,
+    },
+    process.env.JWT_PRIVATE_KEY
+  )
+  return token
+}
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
